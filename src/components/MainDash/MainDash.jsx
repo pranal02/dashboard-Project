6570e56ea70b55cbc7948873ef8d1152b28eaa2d
Pranal Wankhade <pranalwankhade002@gmail.com>
2024-03-15 03:09:34 +0530

    maindash update

--- src/components/MainDash/MainDash.jsx
+++ src/components/MainDash/MainDash.jsx
@@ -5,7 +5,7 @@ import "./MainDash.css";
 const MainDash = () => {
   return (
     <div className="MainDash">
-      <h1>ERP Dashboard</h1>
+      <h1>Dashboard</h1>
       <Cards />
       <Table />
     </div>
