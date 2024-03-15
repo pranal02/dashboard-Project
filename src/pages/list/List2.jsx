1cff08079256bd9f14fe9573511d90288e0f0564
Pranal Wankhade <pranalwankhade002@gmail.com>
2024-03-15 21:55:47 +0530

    create

---
+++ src/pages/list/List2.jsx
@@ -0,0 +1,18 @@
+import "./list2.scss"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
+import Datatable from "../../components/datatable/Datatable"
+
+const List2 = () => {
+  return (
+    <div className="list">
+      <Sidebar/>
+      <div className="listContainer">
+        <Navbar/>
+        <Datatable/>
+      </div>
+    </div>
+  )
+}
+
+export default List2
