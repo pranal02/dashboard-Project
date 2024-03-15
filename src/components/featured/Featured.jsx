642930c474093554fa56d0eb42a8fbe1e1aa82bc
Pranal Wankhade <pranalwankhade002@gmail.com>
2024-03-15 20:47:57 +0530

    circular bar

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -4,6 +4,7 @@ import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
+import { colors } from "@mui/material";
 
 const Featured = () => {
   return (
@@ -26,21 +27,21 @@ const Featured = () => {
             <div className="itemTitle">Target</div>
             <div className="itemResult negative">
               <KeyboardArrowDownIcon fontSize="small"/>
-              <div className="resultAmount">$12.4k</div>
+              <div className="resultAmount">Rs 12.4k</div>
             </div>
           </div>
           <div className="item">
             <div className="itemTitle">Last Week</div>
             <div className="itemResult positive">
               <KeyboardArrowUpOutlinedIcon fontSize="small"/>
-              <div className="resultAmount">$12.4k</div>
+              <div className="resultAmount">Rs 12.4k</div>
             </div>
           </div>
           <div className="item">
             <div className="itemTitle">Last Month</div>
             <div className="itemResult positive">
               <KeyboardArrowUpOutlinedIcon fontSize="small"/>
-              <div className="resultAmount">$12.4k</div>
+              <div className="resultAmount">Rs 12.4k</div>
             </div>
           </div>
         </div>
